d0eb03acae65e59c49d2556f8f67f7afa97b196b
Reynmor <reynmor@ya.ru>
2023-01-23 20:11:23 +0300

    feat: добавить первую версию

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+const container = document.querySelector('.container');
+const title = document.querySelector('.container__title');
+const button = document.querySelector('.button');
+const buttonText = document.querySelector('.button__text');
+const phrases = [
+  'Суслочайки, общий сбор!',
+  'В очко себе тыкни!',
+  'Когда в Чебы?',
+  'По пиву так по пиву',
+  'Инютино – ВО!',
+  'РЕМОНТ'
+]
+
+const startChanges = function(text) {
+  title.textContent = text;
+  title.classList.add('container__title_active');
+}
+
+const changeText = function(element, text) {
+  element.textContent = text;
+}
+
+const getPhrase = function(list) {
+  const randomNumber = Math.floor(Math.random() * list.length);
+  return list[randomNumber];
+}
+
+button.addEventListener('click', () => {
+  if (title.textContent === 'Абонемент на ремонт') {
+    startChanges('РЕМОНТ');
+  }
+
+  else {
+    changeText(title, getPhrase(phrases));
+  }
+})
